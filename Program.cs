3fe3790674f38d5bc4cf496fab697cf7bf71a317
jm-moore <jmmoore9358@gmail.com>
2022-08-05 12:11:06 -0500

    initial commmit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace VariablesExercise
+{
+    internal class Program
+    {
+        public static object If { get; private set; }
+
+        static void Main(string[] args)
+        {
+            string name1 = "Josh";
+            int age = 24;
+            char middleInitial = 'M';
+            bool isAMale = true;
+            double weight = 165.4;
+
+            string v = $"My name is {name1}. I am {age} years old.";
+
+            Console.WriteLine(v);
+
+            string b = $"My middle initial is {middleInitial}.";
+
+            Console.WriteLine(b);
+
+            if (isAMale)
+            Console.WriteLine("I am a male.");
+
+            Console.WriteLine($"I weigh {weight} pounds.");
+        }
+    }
+}
